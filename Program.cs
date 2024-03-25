a23d5f1040f8337c378949dcecde823998a9b6d0
NoufSalehA <noufsalehc23@gmail.com>
2024-03-25 15:37:48 +0300

    level 1 first class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+public class Item{
+    
+    public string Name{
+        get;
+        set;
+    }
+    private int Quantity{
+        get;
+        set;
+    }
+     private DateTime CreatedDate{
+        get;
+        set;
+    }
+    public Item(string name,int quantity,DateTime createdDate){
+        Name=name;
+        Quantity=quantity;
+        CreatedDate=createdDate;
+
+    }
+}
+class Store{
+
+
+
+    
+}
+
+public static void Main(string[] args)
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
